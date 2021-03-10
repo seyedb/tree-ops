974baad0b7e9858f7ca839e0272c7a81491219ab
seyedb <m.h.banihashemian@gmail.com>
2021-03-10 00:00:40 -0500

    first implementation of graph

---
+++ src/treeOps/graph.py
@@ -0,0 +1,20 @@
+from enum import Enum
+from collections import deque
+
+class node_status(Enum):
+    '''
+    List of all possible visit status of each node (graph traversal)
+    '''
+    UNVISITED = 0
+    VISITED = 1
+    VISITING = 2
+
+class graph(object):
+    def __init__(self, vertices={}):
+        self.vertices = vertices
+
+    class graphNode(object):
+        def __init__(self, data=None, children={}, status=node_status.UNVISITED):
+            self.data = data
+            self.children = children
+            self.status = status
